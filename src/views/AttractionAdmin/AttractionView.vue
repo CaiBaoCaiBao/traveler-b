e79c9f245a7f2beb0e208aef3bab34e83fa3635d
CaiBaoCaiBao <13503089560@163.com>
2025-06-04 22:55:05 +0800

    style:setting up the page structure

--- src/views/AttractionAdmin/AttractionView.vue
+++ src/views/AttractionAdmin/AttractionView.vue
@@ -1,13 +1,22 @@
 <template>
-<Container>
-    
-</Container>
+    <Container>
+        <div class="attraction-view">
+            <SideBar />
+            <section>
+                sajkfhjgtdgujdlguyuiofdsgydsoigusad
+            </section>
+        </div>
+    </Container>
 </template>
 
 <script setup lang="ts">
 import Container from "@/components/Container.vue";
+import SideBar from "./SideBar.vue";
 </script>
 
 <style scoped>
-
+.attraction-view {
+    display: grid;
+    grid-template-columns: auto 1fr;
+}
 </style>
