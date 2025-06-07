795847bcea5375b139a09c98e7cb84ada662cadf
CaiBaoCaiBao <13503089560@163.com>
2025-06-07 18:11:42 +0800

    feat:create card commponents

---
+++ src/components/Card.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="card">
+        <div class="card__header" v-if="Title">
+            <Mate :title=Title />
+        </div>
+        <div class="card__body" v-if="Content">
+            <Mate :contnet=Content />
+        </div>
+        <div class="card__footer">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import Mate from '@/components/Mate.vue';
+const props = defineProps({
+    Title: {
+        type: String,
+        defautl: ""
+    },
+    Content: {
+        type: String,
+        defautl: ""
+    }
+})
+</script>
+
+<style scoped>
+.card {
+    padding: 20px;
+    border: 2px solid rgba(42, 88, 187, 0.571);
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    background-color: rgba(6, 4, 71, 0.571);
+}
+
+.card__header {
+    font-weight: bolder;
+    margin-bottom: 10px;
+}
+
+.card__body {
+    margin-bottom: 10px;
+}
+
+.card__footer {
+    width: 100%;
+}
+</style>
