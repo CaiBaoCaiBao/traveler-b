fa7e617a5508837372f049743cbb487ee27a0b39
CaiBaoCaiBao <13503089560@163.com>
2025-06-07 18:13:44 +0800

    feat:create tag commponents

---
+++ src/components/Tag.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="tag">
+        <span>
+            <slot></slot>
+        </span>
+    </div>
+</template>
+
+<style scoped>
+.tag {
+    padding: 5px 10px;
+    background-color: rgba(35, 194, 133, 0.2);
+    border: 1.5px solid rgba(88, 237, 180, 0.3);
+    border-radius: 8px;
+    color: #5eead4;
+    font-size: 14px;
+    font-weight: 500;
+    user-select: none;
+}
+</style>
