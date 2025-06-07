f0e7411404da5c3de79ae839cfbdb0a3a3bb0082
CaiBaoCaiBao <13503089560@163.com>
2025-06-07 18:13:18 +0800

    feat:create mate commponents

---
+++ src/components/Mate.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="mate">
+        <div class="title">
+            {{ title }}
+        </div>
+        <div class="content">
+            {{ contnet }}
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+    title: {
+        type: String,
+    },
+    contnet: {
+        type: String,
+    }
+})
+</script>
+
+<style scoped>
+.title {
+    font-size: 20px;
+    font-weight: bold;
+    margin-bottom: 5px;
+}
+
+.content {
+    font-size: 16px;
+}
+</style>
