b82157cf44505c151a7d592fab1ac857a16acf9c
CaiBaoCaiBao <13503089560@163.com>
2025-06-10 00:03:35 +0800

    feat:table component container

---
+++ src/components/Table/TableComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="table-container">
+        <div class="table-header">
+            <slot></slot>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { provide, readonly } from "vue";
+interface Props<T> {
+    data: Array<T>
+}
+const props = defineProps<Props<any>>();
+// 将传入的data通过provide传递给子组件
+provide("columnProps", readonly(props.data))
+
+</script>
+
+<style scoped>
+.table-container {
+    width: 100%;
+}
+
+.table-header {
+    display: flex;
+    justify-content: space-around;
+}
+</style>
